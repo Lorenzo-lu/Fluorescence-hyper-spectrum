0e3438fb19b2ab41471257d5fc99e23c2182bd94
Lorenzo-lu <ylu289@wisc.edu>
2020-04-08 11:38:35 -0500

    New function added

--- Compressed sensing simulation(PY Version)/PMT_compressed_sensing_simu.py
+++ Compressed sensing simulation(PY Version)/PMT_compressed_sensing_simu.py
@@ -43,11 +43,31 @@ class Compressed_sensing_PMT:
 
     ##+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     ## Generate the Frequence Domain; I choose DCTMTX here. 
-    def DCT_Matrix(self, N):
-        
+    def DCT_Matrix(self, N):        
         I = np.eye(N);
         dctmtx = dct(I, axis=0, norm = 'ortho');
         return dctmtx;   
+    def IDCT_Matrix(self, N):
+        I = np.eye(N);
+        idctmtx = idct(I, axis=0, norm = 'ortho');
+        return idctmtx;  
+        
+    def DCT_Basis(self,r_tot,c_tot): ## This is used to translate idct2 into a matrix tansforming a img vector
+        #D1 = np.linalg.pinv(self.DCT_Matrix(r_tot));
+        #D2 = np.linalg.pinv(self.DCT_Matrix(c_tot));
+        D1 = self.IDCT_Matrix(r_tot);
+        D2 = self.IDCT_Matrix(c_tot);
+        N = r_tot * c_tot;
+        Basis = np.zeros((N,N));
+        i = 0;
+        for ir in range(r_tot): ## rows in raw img
+            for ic in range(c_tot): ## cols in raw img
+                Basis_Vector = D1[ir:ir+1, :].T.dot(D2[ic:ic+1, :]);
+                Basis[i,:] = np.reshape(Basis_Vector, (-1));
+                i += 1;
+        return Basis;                
+        
+        
     ##+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++    
     ##+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ 
     ## making the DMD patterns and measurement basis    
@@ -69,14 +89,19 @@ class Compressed_sensing_PMT:
         res_index = np.where(freq_pattern[:r1+1, :c1+1] == 0);
         res_index = np.array(res_index);  
         
-        index_sum = np.vstack((res_index, res_index[0,:] + res_index[1,:]));
+        index_sum = np.vstack((res_index, res_index[0,:] / (r1+1) + res_index[1,:] / (c1+1)));
+        ## The last row is the weighted Manhatan Distance:
+        ## The selection for the residual candidates should start from low freq to high freq
+        ## The weights are set to neutralize the impacts of the rectangle shape of a picture
+        ## make it look like in a square coordinate system;
+        ## so (r) / (r1+1) and (c) / (r1+1)
         
         
         sorted_index = index_sum[:,index_sum[2].argsort()];
         ## sort the list by the sum of x and y index :: quicker than for loop?       
         
         #np.random.shuffle(res_index.T);
-        selection = sorted_index[0:2,0:n_res];   
+        selection = sorted_index[0:2,0:n_res].astype(int);   
         
         freq_pattern[tuple(selection)] = 1;
                   
@@ -87,7 +112,7 @@ class Compressed_sensing_PMT:
         
         return freq_pattern;
     
-    def DMD_pattern(self,r,c, size = False):
+    def DMD_pattern(self,r,c, size = False): ## 0,1 matrix by idct2
         #x_r = np.array(range(r_tot)) / r_tot * np.pi;
         #x_c = np.array(range(c_tot)) / c_tot * np.pi;
         #y_r = np.cos(x_r * r) * 0.5; ## to avoid -1. we only need 0 and 1
@@ -107,7 +132,8 @@ class Compressed_sensing_PMT:
         
         return np.ceil(DCT_pattern);
     
-    def DMD_measure_basis(self, freq_pattern):
+    def DMD_measure_basis(self, freq_pattern): 
+        ## reshape each DMD_pattern in to a row in measure matrix
         index = np.where(freq_pattern == 1);
         index = np.array(index).T;
         r_tot, c_tot = freq_pattern.shape;
@@ -128,14 +154,15 @@ class Compressed_sensing_PMT:
         
     def Gaussian_noise(self,array, SNR = False):
         if not SNR:
-            SNR = 100;            
-        std = np.mean(array) * SNR;            
+            SNR = 100;  
+        
+        std = np.mean(array) / SNR;            
         return np.random.normal(array,std);
     
     ##+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++    
     ##+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++     
     ## for simulation                 
-    def Load_img(self, img_pathname = False, RGB = False, size = False):
+    def Load_img(self, img_pathname = False, RGB = False, size = False, anti_aliasing = True):
         if not img_pathname:
             img_pathname = input("Please input the pathname of the image you want to test.\n");
         m = mpimg.imread(img_pathname);
@@ -149,7 +176,7 @@ class Compressed_sensing_PMT:
         if np.all(size):
             r = size[0];
             c = size[1];            
-            m = resize(m, (r, c),anti_aliasing = True);
+            m = resize(m, (r, c),anti_aliasing);
             
         self.img = m / np.max(m);
         if len(self.img.shape) == 3:
@@ -171,6 +198,7 @@ class Compressed_sensing_PMT:
             self.n_photons = n_photons;
         else:
             print("You should input the number of photons as a number, list or array");
+        
             
     def PMT_measure_simu(self, photon_number, cycles, Poisson = False, Gaussian = False):
         ## Photon Number investigates the impacts of the Intensity        
@@ -180,7 +208,7 @@ class Compressed_sensing_PMT:
             img_vector = np.zeros((r_tot*c_tot,1))
             img_vector[:,0] = np.reshape(self.img, (r_tot*c_tot));## make img a vector
             
-        elif len(self.img.shape) == 3:
+        elif len(self.img.shape) == 3: ## make RGB image a n*3 matrix
             r_tot,c_tot = self.img[:,:,0].shape;
             img_vector = np.zeros((r_tot*c_tot,3))
             
@@ -190,9 +218,14 @@ class Compressed_sensing_PMT:
             print("Wrong image shape");
             return;
         
-        measurement = np.zeros((cycles, len(img_vector[0,:])));
+        img_vector *= photon_number; ## how bright the environment is
         
-        self.DMD_measure_basis(self.DMD_Freq_pattern(r_tot, c_tot, cycles));
+        
+        measurement = np.zeros((cycles, len(img_vector[0,:]))); ## record the measured intensity
+        
+        self.DMD_measure_basis(self.DMD_Freq_pattern(r_tot, c_tot, cycles)); 
+        ## That function has no return:: it make the matrix of self.DMD_basis, which means
+        ## cycles x measurements in vector form
         
         
         for layer in range(len(img_vector[0,:])):
@@ -200,39 +233,58 @@ class Compressed_sensing_PMT:
             measurement_layer = [];
             for i in range(len(self.DMD_basis)):   
                 
-                val = self.DMD_basis[i,:].dot(img_vector[:,layer]) * photon_number;
+                val = self.DMD_basis[i,:].dot(img_vector[:,layer]);
                 if Poisson:
                     val = self.Poisson_noise(val);
-                measurement_layer.append(val);
-            measurement_layer = np.array(measurement_layer);            
                 
-            if Gaussian:
-                SNR = float(input("Please input the Signal to Noise Ratio (Type::Float):"));
-                measurement_layer = ( self.Gaussian_noise(measurement_layer, SNR));
+                measurement_layer.append(val);
+            measurement_layer = np.array(measurement_layer);
                
             measurement[:,layer] = measurement_layer;
+            
+        if Gaussian != False:## Gaussian noise from current, so added at the final
+            if Gaussian == 'Poisson': ## Use Gaussian to simulate Poisson
+                SNR = (np.mean(measurement))**0.5;
+                measurement = ( self.Gaussian_noise(measurement, SNR));
+            
+            else:            
+                choice = int(input("The noise is in which scale \n1. Relative 2. Abstact\n"));
+                if choice == 1:
+                    SNR = float(input("Please input the Signal to Noise Ratio (Type::Float):\n"));
+                elif choice == 2:
+                    noise_amplitude = float(input("Please input the noise amplitude (Type::Float):\n"));
+                    SNR = np.mean(measurement) / noise_amplitude;
+                measurement = ( self.Gaussian_noise(measurement, SNR));
         
         measurement /= photon_number;
-        return measurement;
+        return measurement;  
+
         
         
-    def PMT_reconstruct(self, r_tot, c_tot, measurement):
+    #--------------------------------------------------------------------------
+    # using gradient descent    
+    def PMT_reconstruct(self, r_tot, c_tot, measurement, user_evaluation = True,\
+                        learning_rate =  5e-4, beta = 0.5, regularization = 0, plot_step = 1000, max_epoch = False, batch_size = 10):
         
-        FT_Basis = self.DCT_Matrix(r_tot*c_tot);
+        #self.FT_Basis = self.IDCT_Matrix(r_tot*c_tot);
+        self.FT_Basis = self.DCT_Basis(r_tot,c_tot);
         
         cycles = len(measurement);
         if not np.all(self.DMD_basis):
             self.DMD_measure_basis(self.DMD_Freq_pattern(r_tot, c_tot, cycles));
         ## get self.DMD_basis;
         
-        self.Measure_matrix = self.DMD_basis.dot(FT_Basis);
+        self.Measure_matrix = self.DMD_basis.dot(self.FT_Basis);
         self.W = np.zeros((r_tot*c_tot,len(measurement[0,:]))); ## FT domain coef
         self.measurement = measurement;
         
         ## Find the optimal
-        self.Optimal(learning_rate = 5e-4, beta = 0.5,batch_size = 10);
+        if user_evaluation:
+            self.Optimal(learning_rate, beta, regularization, plot_step, max_epoch, batch_size);
+        else:
+            self.Gradient_Ascent(learning_rate, beta, regularization, plot_step, max_epoch, batch_size);
         
-        self.img_reconstructed_vector = FT_Basis.dot(self.W);
+        self.img_reconstructed_vector = self.FT_Basis.dot(self.W);
         
         self.img_reconstructed = np.zeros((r_tot, c_tot, len(measurement[0,:])));
         
@@ -248,11 +300,12 @@ class Compressed_sensing_PMT:
         
         plt.figure();
         plt.imshow(self.img_reconstructed);
-        plt.show();
+        plt.show();   
         
+    def Gradient_Ascent(self,learning_rate, beta = 0, regularization = 0, plot_step = 1000, max_epoch = 50000, batch_size = 'all'):
+        if not max_epoch:
+            max_epoch = plot_step * 50;
         
-        
-    def Gradient_Ascent(self,learning_rate, beta = 0, regularization = 0, plot_step = 1000, max_epoch = False, batch_size = 'all'):
         if type(batch_size) == int:
             if batch_size == 1:
                 print("Stochastic Gradient Ascent!");
@@ -270,10 +323,8 @@ class Compressed_sensing_PMT:
                     rnd_indices = np.random.randint(0, len(perm), size);
                     selection = perm[rnd_indices];
 
-
                     x = np.reshape(selection[:,:cols],(size,-1));
-                    y = np.reshape(selection[:,cols:],(size,-1));
-                    
+                    y = np.reshape(selection[:,cols:],(size,-1));                    
 
                     return x,y;
             return self.Measure_matrix,self.measurement;
@@ -365,14 +416,59 @@ class Compressed_sensing_PMT:
                         para_map[index] = value;
                     
                     Adjust = input("ALL SET?\nY/N\n");
+            
+    #--------------------------------------------------------------------------
+    # non gradient descent method
+    def Get_direct_inverse(self, r_tot, c_tot,  regularization = 0):  
+            self.FT_Basis = self.DCT_Basis(r_tot,c_tot);
+            self.Measure_matrix = self.DMD_basis.dot(self.FT_Basis);
+            
+            M = np.linalg.pinv(self.Measure_matrix.dot(self.Measure_matrix.T));
+            
+            M += regularization * np.eye(M.shape[0],M.shape[1]);                           
+            
+            M = self.Measure_matrix.T.dot(M);  
+
+            self.direct_inverse_M = M;            
+                    
+    def PMT_direct_inverse_reconstruction(self, r_tot, c_tot, M, measurement):
+        
+        #def Make_direct_inverse(r_tot, c_tot):
+            #self.FT_Basis = self.DCT_Basis(r_tot,c_tot);
+            #self.Measure_matrix = self.DMD_basis.dot(self.FT_Basis);
+            
+            #M = np.linalg.pinv(self.Measure_matrix.dot(self.Measure_matrix.T));
+            
+            #M += regularization * np.eye(M.shape[0],M.shape[1]);                           
+            
+            #M = self.Measure_matrix.T.dot(M); 
+            #return M;
+        
+        #M = np.linalg.pinv(self.Measure_matrix);
+        
+        self.W = M.dot(measurement);
+        self.img_reconstructed_vector = self.FT_Basis.dot(self.W);        
+        self.img_reconstructed = np.zeros((r_tot, c_tot, len(measurement[0,:])));
+        
+        for i in range(len(measurement[0,:])):
+            self.img_reconstructed[:,:,i] = np.reshape(self.img_reconstructed_vector[:,i], (r_tot, c_tot));
+            
+        if len(measurement[0,:]) == 1:
+            self.img_reconstructed = self.img_reconstructed[:,:,0];            
+        self.img_reconstructed = np.clip(self.img_reconstructed, a_min = 0,a_max = 1);        
+        plt.figure();
+        plt.imshow(self.img_reconstructed);
+        plt.show();
 
         
         
-        
-        
+'''        
+r = 100;
+c = 100;
+   
 a = Compressed_sensing_PMT();
-a.Load_img("ECE.jpg",RGB = True, size = (100,100));        
-measure = a.PMT_measure_simu(1000,2000, Poisson = True);        
-a.PMT_reconstruct(100,100,measure);        
-        
+a.Load_img("ECE.jpg",RGB = True, size = (r,c), anti_aliasing = False);        
+measure = a.PMT_measure_simu(1000,round(r*c/5), Poisson = False);        
+a.PMT_reconstruct(r,c,measure);        
+'''        
         
